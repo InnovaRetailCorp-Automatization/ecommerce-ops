212690e6f9c7fe79e6eea161e7061be9993bbf10
Jhorman Mera <1010159945@u.icesi.edu.co>
2024-05-17 20:45:27 -0500

    aks module

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,14 +40,16 @@ module "ip_appgw" {
   source = "./modules/public_ip"
   name = "${var.prefix_name}-ip-appgw"
   location = var.region
+  resource_group_name = module.resource_group.name
   allocation_method = "Static"
   sku = "Basic"
 }
 #Llamado al modulo de la Ip Pública del bastion
 module "ip_bastion" {
-  source = "./modules/bastion"
+  source = "./modules/public_ip"
   name = "${var.prefix_name}-ip-bastion"
   location = var.region
+  resource_group_name = module.resource_group.name
   allocation_method = "Static"
   sku = "Basic"
 }
@@ -64,23 +66,10 @@ module "ip_bastion" {
 
 # Llamado al modulo para la creación del clúster de AKS
 module "aks_cluster" {
-  source                  = "./modules/aks_cluster"
-  name                    = "${var.prefix_name}-aks"
-  location                = module.resource_group.location
-  resource_group_name     = module.resource_group.resource_group_name
-  dns_prefix              = "${var.prefix_name}-aks"
-  node_pool_name          = "default"
-  node_count              = 1
-  vm_size                 = "Standard_D2_v2"
-  os_disk_size_gb         = 40
-  vnet_subnet_id          = module.appgw_subnet.id
-  network_plugin          = "azure"
-  identity_type           = "SystemAssigned"
-  local_file_name         = "kubeconfig"
-  secret_rotation_enabled = true
-  private_cluster_enabled = true
+  source = "./modules/aks_cluster"
+  name ="${var.prefix_name}-aks"
+  location = module.resource_group.location
+  resource_group_name = module.resource_group.name
+  dns_prefix = "${var.prefix_name}-aks"
+  local_file = "KubeConfig"
 }
-
-
-
-
