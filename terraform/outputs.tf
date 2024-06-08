ee458d7136e4065ddf313a50113cf471a4fe2bff
Jhorman Mera <1010159945@u.icesi.edu.co>
2024-06-08 11:36:47 -0500

    outputs updated

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,11 @@ output "application_gateway_name" {
 output "cluster_name" {
   value = module.aks_cluster.name
 }
+
+output "cr_name" {
+  value = module.container_registry.name
+}
+
+output "identity_client_id" {
+  value = module.identity.client_id
+}
